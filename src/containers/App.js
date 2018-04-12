a59827ab4cca1d31c385d8ff08588d73be0bba16
Johan Roug <jro@addition.dk>
2018-04-12 09:36:42 +0200

    no message

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,12 +18,8 @@ class App extends Component {
   }
 
   render () {
-    // const posts = this.state.posts.map(items => <li key={items.id}>{items.title}</li>);
     return (
       <div>
-        {/* <ul className='App'>
-          {posts}
-        </ul> */}
         <AuthContext.Provider value={this.state}>
           <Child />
         </AuthContext.Provider>
