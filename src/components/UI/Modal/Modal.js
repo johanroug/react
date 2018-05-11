40b73f0288fb90577e4cefac66f3c69eefac6a69
Johan Roug <jro@addition.dk>
2018-05-12 00:22:49 +0200

    Navigation

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -9,7 +9,8 @@ const modal = (props) => {
       <div className={classes.Modal}
         style={{
           transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: props.show ? 1 : 0
+          opacity: props.show ? 1 : 0,
+          zIndex: props.show ? 500 : -1
         }}>
         <div onClick={props.close}>close</div>
         {props.children}
