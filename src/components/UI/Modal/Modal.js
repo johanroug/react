165c0acf857cb28f3b6f1d5873843d888d394788
Johan Roug <jro@addition.dk>
2018-05-14 10:44:04 +0200

    Only update modal if visible

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import classes from './Modal.scss';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 
-const modal = (props) => {
-  return (
-    <Aux>
-      <div className={classes.Modal}
-        style={{
-          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: props.show ? 1 : 0,
-          zIndex: props.show ? 500 : -1
-        }}>
-        <div onClick={props.close}>close</div>
-        {props.children}
-      </div>
-      <Backdrop show={props.show} close={props.close} />
-    </Aux>
-  );
-};
+class Modal extends Component {
+  shouldComponentUpdate (nextProps) {
+    return nextProps.show !== this.props.show;
+  }
+  render () {
+    return (
+      <Aux>
+        <div className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? 1 : 0,
+            zIndex: this.props.show ? 500 : -1
+          }}>
+          <div onClick={this.props.close}>close</div>
+          {this.props.children}
+        </div>
+        <Backdrop show={this.props.show} close={this.props.close} />
+      </Aux>
+    );
+  }
+}
 
-export default modal;
+export default Modal;
